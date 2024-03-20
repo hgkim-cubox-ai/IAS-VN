7a6c7f0168679c75930331c1a4b94abfa3cafc3f
heegyoon <hgkim@cubox.ai>
2024-03-20 16:41:55 +0900

    update

--- preprocessing/label_gt.py
+++ preprocessing/label_gt.py
@@ -4,6 +4,8 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import json
 import shutil
 from glob import glob
+import cv2
+import numpy as np
 
 from utils import is_image_file
 
@@ -16,21 +18,25 @@ def copy_images_in_directory(path, label, output_path):
     
     for img_file in img_files:
         ext = os.path.splitext(img_file)[1]
-        shutil.copy(os.path.join(path, img_file),
-                    os.path.join(output_path, f'{img_idx:08d}{ext}'))
+        src = os.path.join(path, img_file)
+        dst = os.path.join(output_path, f'{img_idx:08d}{ext}')
+        shutil.copy(src, dst)
         annot = {'spoof_label': label}
         with open(os.path.join(output_path, f'{img_idx:08d}.json'), 'w') as f:
-            json.dump(annot, f, ensure_ascii=False)
+            json.dump(annot, f, indent=4, ensure_ascii=False)
         img_idx += 1
         
-    
-    # for img_path in img_paths:
-    #     label_path = os.path.splitext(img_path)[0] + '.json'
+        # Debugging
+        s = np.fromfile(src, np.uint8)
+        s = cv2.imdecode(s, cv2.IMREAD_COLOR)
+        d = cv2.imread(dst)
+        if len(np.where(s!=d)[0]) != 0:
+            print(s)
 
 
 def shinhan_poc():
     input_path = 'C:/Users/heegyoon/Desktop/data/IAS/raw/shinhan_poc/14시/크롭 이미지'
-    output_path = 'C:/Users/heegyoon/Desktop/data/IAS/raw/shinhan'
+    output_path = 'C:/Users/heegyoon/Desktop/data/IAS/crop/shinhan'
     os.mkdir(output_path)
     sub_dirs1 = os.listdir(input_path)   # 사본 또는 원본 폴더
     label = None
@@ -55,17 +61,7 @@ def shinhan_poc():
                 else:   # 진본
                     label = 1
                     copy_images_in_directory(sub_path3, label, output_path)
-            
-    
-    
-    
-    print('')
-    pass
 
-
-def main():
-    
-    pass
 
 
 if __name__ == '__main__':
