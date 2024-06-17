c50b49e7e756e7a973bb84d817bf5c01f5209f69
heegyoon <hgkim@cubox.ai>
2024-06-17 13:55:40 +0900

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,10 @@
 from .ias_model import IASModel
 from .lbp_model import LBPModel
+from .resnet_model import ResNetModel
 
 
 MODEL_DICT = {
     'ias_model': IASModel,
     'lbp_model': LBPModel,
+    'resnet_model': ResNetModel
 }
---
+++ models/resnet_model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+from .backbone import CNN_ResNet
+
+
+class ResNetModel(nn.Module):
+    def __init__(self, cfg):
+        super(ResNetModel, self).__init__()
+        
+        self.cfg = cfg
+        self.backbone = CNN_ResNet(cfg['backbone'])
+        self.make_regressor()
+    
+    def make_regressor(self):
+        fc_in = 2048 if self.cfg['backbone'] > 'resnet18' else 512
+        
+        regressor = []
+        regressor.append(nn.Linear(fc_in, self.cfg['regressor'][0]))
+        for i in range(len(self.cfg['regressor'])-1):
+            regressor.append(nn.ReLU(inplace=True))
+            regressor.append(nn.Linear(self.cfg['regressor'][i],self.cfg['regressor'][i+1]))
+        regressor.append(nn.Sigmoid())
+        self.regressor = nn.Sequential(*regressor)
+    
+    def forward(self, img):
+        feat = self.backbone(img)
+        feat = feat.view(feat.size(0), -1)
+        feat = self.regressor(feat)
+        return feat
