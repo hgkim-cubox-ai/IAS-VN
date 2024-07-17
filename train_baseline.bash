7d2194c037070376f3389c72f0fed62c3709a9f9
heegyoon <hgkim@cubox.ai>
2024-07-17 12:09:08 +0900

    update

--- train_baseline.bash
+++ train_baseline.bash
@@ -2,8 +2,15 @@
 # torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/34_0.0001.yaml
 
 # ResNet-50
-torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/50/0.01.yaml
-torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/50/0.005.yaml
-torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/50/0.001.yaml
-torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/50/0.0005.yaml
+# torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/50/0.01.yaml
+# torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/50/0.005.yaml
+# torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/50/0.001.yaml
+# torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/50/0.0005.yaml
 # torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/50/0.0001.yaml
+
+# ResNet-101
+torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/101/0.01.yaml
+torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/101/0.005.yaml
+torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/101/0.001.yaml
+torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/101/0.0005.yaml
+torchrun --nnodes=1 --nproc_per_node=4 main.py --cfg experiments/baseline/101/0.0001.yaml
