e85d35028f0319e5a4e1ac4226b6075fcf143a32
heegyoon <hgkim@cubox.ai>
2024-07-23 10:19:32 +0900

    update

--- train_baseline.bash
+++ train_baseline.bash
@@ -21,6 +21,8 @@
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.0010.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.00005.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.00001.yaml
+# 300 epoch
+torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.001.yaml
 
 # 101
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/101/0.005.yaml
@@ -31,7 +33,7 @@
 # torchrun --nnodes=1--nproc_per_node=1 main.py --cfg experiments/baseline/101/0.00001.yaml
 
 # 152
-torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/152/0.001.yaml
+# torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/152/0.001.yaml
 
 # without bias of regressor
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50_wo_fc_bias/0.001.yaml
