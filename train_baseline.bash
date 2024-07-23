bfbb36423cb17e33338e9ac4c6fb25690f960802
heegyoon <hgkim@cubox.ai>
2024-07-23 10:34:36 +0900

    update

--- train_baseline.bash
+++ train_baseline.bash
@@ -1,6 +1,7 @@
 # 18
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/18/0.005.yaml
 torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/18/0.001.yaml
+sleep 60
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/18/0.0005.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/18/0.0010.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/18/0.00005.yaml
@@ -9,6 +10,7 @@ torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/18/0.0
 # 34
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/34/0.005.yaml
 torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/34/0.001.yaml
+sleep 60
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/34/0.0005.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/34/0.0010.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/34/0.00005.yaml
@@ -17,6 +19,7 @@ torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/34/0.0
 # 50
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.005.yaml
 torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.001.yaml
+sleep 60
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.0005.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.0010.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.00005.yaml
@@ -27,6 +30,7 @@ torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/50/0.0
 # 101
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/101/0.005.yaml
 torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/101/0.001.yaml
+sleep 60
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/101/0.0005.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/101/0.0010.yaml
 # torchrun --nnodes=1 --nproc_per_node=1 main.py --cfg experiments/baseline/101/0.00005.yaml
